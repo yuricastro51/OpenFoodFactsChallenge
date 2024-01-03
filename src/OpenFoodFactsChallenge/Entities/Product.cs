1010e84e1a5dee0ecb629f1265719b63b171716f
Yuri Castro <yuricastro51@gmail.com>
2024-01-02 22:46:06 -0300

    feat: ensure Product can be instantiated

---
+++ src/OpenFoodFactsChallenge/Entities/Product.cs
@@ -0,0 +1,36 @@
+﻿namespace OpenFoodFactsChallenge.Entities;
+
+public class Product(long code, 
+    string barcode, 
+    string url, 
+    string productName, 
+    string quantity, 
+    string categories, 
+    string packaging, 
+    string brands, 
+    string imageUrl)
+{
+    public long Code { get; private set; } = code;
+    public string Barcode { get; private set; } = barcode;
+    public EStatus Status { get; private set; } = EStatus.Draft;
+    public DateTime ImportedT { get; private set; }
+    public string Url { get; private set; } = url;
+    public string ProductName { get; private set; } = productName;
+    public string Quantity { get; private set; } = quantity;
+    public string Categories { get; private set; } = categories;
+    public string Packaging { get; private set; } = packaging;
+    public string Brands { get; private set; } = brands;
+    public string ImageUrl { get; private set; } = imageUrl;
+
+    public void SetImported()
+    {
+        ImportedT = DateTime.Now;
+        Status = EStatus.Imported;
+    }
+}
+
+public enum EStatus
+{
+    Draft,
+    Imported
+}
---
+++ tests/OpenFoodFactsChallenge.Tests/Entities/ProductTests.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using OpenFoodFactsChallenge.Entities;
+
+namespace OpenFoodFactsChallenge.Tests.Entities;
+
+public class ProductTests
+{
+    [Fact]
+    public void Instantiate()
+    {
+        var validData = new
+        {
+            Code = 1L, 
+            Barcode = "1234567890123", 
+            Url = "https://world.openfoodfacts.org/product/1234567890123", 
+            ProductName = "Product Name", 
+            Quantity = "1 kg", 
+            Categories = "Category 1, Category 2", 
+            Packaging = "Packaging 1, Packaging 2", 
+            Brands = "Brand 1, Brand 2", 
+            ImageUrl = "https://static.openfoodfacts.org/images/products/1234567890123/front_en.123.400.jpg"
+        };
+        
+
+        var product = new Product(
+            validData.Code, 
+            validData.Barcode, 
+            validData.Url, 
+            validData.ProductName,
+            validData.Quantity, 
+            validData.Categories, 
+            validData.Packaging, 
+            validData.Brands, 
+            validData.ImageUrl);
+
+        product.Should().NotBeNull();
+        product.Code.Should().Be(validData.Code);
+        product.Barcode.Should().Be(validData.Barcode);
+        product.Url.Should().Be(validData.Url);
+        product.ProductName.Should().Be(validData.ProductName);
+        product.Quantity.Should().Be(validData.Quantity);
+        product.Categories.Should().Be(validData.Categories);
+        product.Packaging.Should().Be(validData.Packaging);
+        product.Brands.Should().Be(validData.Brands);
+        product.ImageUrl.Should().Be(validData.ImageUrl);
+        product.Status.Should().Be(EStatus.Draft);
+        product.ImportedT.Should().Be(default);
+    }
+
+    [Fact]
+    public void ShouldSetStatusToImported()
+    {
+        var validData = new
+        {
+            Code = 1L, 
+            Barcode = "1234567890123", 
+            Url = "https://world.openfoodfacts.org/product/1234567890123", 
+            ProductName = "Product Name", 
+            Quantity = "1 kg", 
+            Categories = "Category 1, Category 2", 
+            Packaging = "Packaging 1, Packaging 2", 
+            Brands = "Brand 1, Brand 2", 
+            ImageUrl = "https://static.openfoodfacts.org/images/products/1234567890123/front_en.123.400.jpg"
+        };
+        
+
+        var product = new Product(
+            validData.Code, 
+            validData.Barcode, 
+            validData.Url, 
+            validData.ProductName,
+            validData.Quantity, 
+            validData.Categories, 
+            validData.Packaging, 
+            validData.Brands, 
+            validData.ImageUrl);
+
+        product.SetImported();
+
+        product.Status.Should().Be(EStatus.Imported);
+        product.ImportedT.Should().NotBe(default);
+    }
+}
