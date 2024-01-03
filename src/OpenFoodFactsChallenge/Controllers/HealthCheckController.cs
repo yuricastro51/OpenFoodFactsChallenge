1f507e5a95ec4a92b16e64c321a586d3a6524429
Yuri Castro <yuricastro51@gmail.com>
2024-01-02 23:08:50 -0300

    feat: ensure HealthCheckController returns 200

---
+++ src/OpenFoodFactsChallenge/Controllers/HealthCheckController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenFoodFactsChallenge.Controllers
+{
+    [ApiController]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    public class HealthCheckController : Controller
+    {
+        [HttpGet]
+        [Route("/")]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        public IActionResult HealthCheck()
+        {
+            return Ok("Fullstack Challenge 20201026");
+        }
+    }
+}
---
+++ tests/OpenFoodFactsChallenge.Tests/Controllers/HealthCheckControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using OpenFoodFactsChallenge.Controllers;
+
+namespace OpenFoodFactsChallenge.Tests.Controllers;
+
+public class HealthCheckControllerTest
+{
+    [Fact]
+    public void HealthCheck_ReturnsOk()
+    {
+        var controller = new HealthCheckController();
+
+        var result = (OkObjectResult)controller.HealthCheck();
+
+        result.StatusCode.Should().Be(200);
+        result.Value.Should().Be("Fullstack Challenge 20201026");
+    }
+}
